f2d8802b9c027e93d7b33f31be9059c8cc76dc90
PurushothamaGovindaraju <purushothama@octalogic.in>
2025-06-24 08:47:41 +0530

    Basic set up

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.local', '.env.production'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      entities: [__dirname + '/entity/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+  ],
+})
+export class AppModule {}
